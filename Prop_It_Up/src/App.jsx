8b8213ac0473ab5612a3d3e56bd9fbbb0fd62dba
FJORALB10 <fjoralbtaraj@gmail.com>
2023-09-04 01:27:48 +0200

    second commit-putting it together

--- Prop_It_Up/src/App.jsx
+++ Prop_It_Up/src/App.jsx
@@ -4,10 +4,10 @@ import PersonCard from './assets/components/PersonCard';
 function App() {
   return (
   <>
-    <PersonCard firstName={'Jane'} lastName={'Doe'} age={'45'} hairColor={'black'} />
-    <PersonCard firstName={'John'} lastName={'Smith'} age={'88'} hairColor={'Brown'} />
-    <PersonCard firstName={'Millard'} lastName={'Fillmore'} age={'50'} hairColor={'Brown'} />
-    <PersonCard firstName={'Maria'} lastName={'Smith'} age={'62'} hairColor={'Brown'} />
+    <PersonCard firstName={'Jane'} lastName={'Doe'} age={45} hairColor={'black'} />
+    <PersonCard firstName={'John'} lastName={'Smith'} age={88} hairColor={'Brown'} />
+    <PersonCard firstName={'Millard'} lastName={'Fillmore'} age={50} hairColor={'Brown'} />
+    <PersonCard firstName={'Maria'} lastName={'Smith'} age={62} hairColor={'Brown'} />
   </>);
 }
 
--- Prop_It_Up/src/assets/components/PersonCard.jsx
+++ Prop_It_Up/src/assets/components/PersonCard.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 const PersonCard = (props)=> {
+    const [age, setAge] = useState(props.age);
+
     return(
         <div className="cards">
             <h1>{props.lastName}, {props.firstName}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {age}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={()=>setAge(age+1)}>Birthday Button for {props.firstName} {props.lastName}</button> 
         </div>
     );
-}
-
+    }
 export default PersonCard;
